dcb4f8c91f608cb77d211ec1dd15e6ca9d94fffe
fathisahar <uhsahar@gmail.com>
2023-03-30 17:16:42 -0400

    new color detection

--- project/color_detection_two.py
+++ project/color_detection_two.py
@@ -1,7 +1,6 @@
-# (R, G, B) average for red from our tests for the DRIVING sensor
 from math import sqrt
 
-
+# (R, G, B) average for red from our tests for the DRIVING sensor
 rR = 245
 gR = 46
 bR = 11
@@ -9,13 +8,6 @@ normrR = rR / sqrt(pow(rR, 2) + pow(gR, 2) + pow(bR, 2))
 normgR = gR / sqrt(pow(rR, 2) + pow(gR, 2) + pow(bR, 2))
 normbR = bR / sqrt(pow(rR, 2) + pow(gR, 2) + pow(bR, 2))
 
-# (R, G, B) average for red from our tests for the DELIVERY sensor
-rRDel = 200
-gRDel = 30
-bRDel = 30
-normrRDel = rRDel / sqrt(pow(rRDel, 2) + pow(gRDel, 2) + pow(bRDel, 2))
-normgRDel = gRDel / sqrt(pow(rRDel, 2) + pow(gRDel, 2) + pow(bRDel, 2))
-normbRDel = bRDel / sqrt(pow(rRDel, 2) + pow(gRDel, 2) + pow(bRDel, 2))
 
 # (R, G, B) average for orange from our tests for the DRIVING sensor
 rO = 265
@@ -25,15 +17,6 @@ normrO = rO / sqrt(pow(rO, 2) + pow(gO, 2) + pow(bO, 2))
 normgO = gO / sqrt(pow(rO, 2) + pow(gO, 2) + pow(bO, 2))
 normbO = bO / sqrt(pow(rO, 2) + pow(gO, 2) + pow(bO, 2))
 
-
-# (R, G, B) average for orange from our tests for the DELIVERY sensor
-rODel = 200
-gODel = 70
-bODel = 20
-normrODel = rODel / sqrt(pow(rODel, 2) + pow(gODel, 2) + pow(bODel, 2))
-normgODel = gODel / sqrt(pow(rODel, 2) + pow(gODel, 2) + pow(bODel, 2))
-normbODel = bODel / sqrt(pow(rODel, 2) + pow(gODel, 2) + pow(bODel, 2))
-
 # (R, G, B) average for yellow from our tests for the DRIVING sensor
 rY = 255
 gY = 230
@@ -42,13 +25,6 @@ normrY = rY / sqrt(pow(rY, 2) + pow(gY, 2) + pow(bY, 2))
 normgY = gY / sqrt(pow(rY, 2) + pow(gY, 2) + pow(bY, 2))
 normbY = bY / sqrt(pow(rY, 2) + pow(gY, 2) + pow(bY, 2))
 
-# (R, G, B) average for yellow from our tests for the DELIVERY sensor
-rYDel = 166
-gYDel = 130
-bYDel = 30
-normrYDel = rYDel / sqrt(pow(rYDel, 2) + pow(gYDel, 2) + pow(bYDel, 2))
-normgYDel = gYDel / sqrt(pow(rYDel, 2) + pow(gYDel, 2) + pow(bYDel, 2))
-normbYDel = bYDel / sqrt(pow(rYDel, 2) + pow(gYDel, 2) + pow(bYDel, 2))
 
 # (R, G, B) average for green from our tests for the DRIVING sensor
 rG= 47
@@ -58,13 +34,6 @@ normrG = rG / sqrt(pow(rG, 2) + pow(gG, 2) + pow(bG, 2))
 normgG = gG / sqrt(pow(rG, 2) + pow(gG, 2) + pow(bG, 2))
 normbG = bG / sqrt(pow(rG, 2) + pow(gG, 2) + pow(bG, 2))
 
-# (R, G, B) average for green from our tests for the DELIVERY sensor
-rGDel = 50
-gGDel= 107
-bGDel= 30
-normrGDel = rGDel / sqrt(pow(rGDel, 2) + pow(gGDel, 2) + pow(bGDel, 2))
-normgGDel = gGDel / sqrt(pow(rGDel, 2) + pow(gGDel, 2) + pow(bGDel, 2))
-normbGDel = bGDel / sqrt(pow(rGDel, 2) + pow(gGDel, 2) + pow(bGDel, 2))
 
 # (R, G, B) average for blue from our tests for the DRIVING sensor
 rB = 34
@@ -73,14 +42,6 @@ bB = 38
 normrB = rB / sqrt(pow(rB, 2) + pow(gB, 2) + pow(bB, 2))
 normgB = gB / sqrt(pow(rB, 2) + pow(gB, 2) + pow(bB, 2))
 normbB = bB / sqrt(pow(rB, 2) + pow(gB, 2) + pow(bB, 2))
-
-# (R, G, B) average for blue from our tests for the DELIVERY sensor
-rBDel = 30
-gBDel = 50
-bBDel = 80.4
-normrBDel = rBDel / sqrt(pow(rBDel, 2) + pow(gBDel, 2) + pow(bBDel, 2))
-normgBDel = gBDel / sqrt(pow(rBDel, 2) + pow(gBDel, 2) + pow(bBDel, 2))
-normbBDel = bBDel / sqrt(pow(rBDel, 2) + pow(gBDel, 2) + pow(bBDel, 2))
 
 # (R, G, B) average for purple from our tests for the DRIVING sensor
 rP = 176
@@ -91,13 +52,6 @@ normgP = gP / sqrt(pow(rP, 2) + pow(gP, 2) + pow(bP, 2))
 normbP = bP / sqrt(pow(rP, 2) + pow(gP, 2) + pow(bP, 2))
 
 
-rPDel = 140
-gPDel = 50.6
-bPDel = 40.6
-normrPDel = rPDel / sqrt(pow(rPDel, 2) + pow(gPDel, 2) + pow(bPDel, 2))
-normgPDel = gPDel / sqrt(pow(rPDel, 2) + pow(gPDel, 2) + pow(bPDel, 2))
-normbPDel = bPDel / sqrt(pow(rPDel, 2) + pow(gPDel, 2) + pow(bPDel, 2))
-
 # (R, G, B) average for white from our tests
 rW = 301
 gW = 289
@@ -106,6 +60,66 @@ normrW = rW / sqrt(pow(rW, 2) + pow(gW, 2) + pow(bW, 2))
 normgW = gW / sqrt(pow(rW, 2) + pow(gW, 2) + pow(bW, 2))
 normbW = bW / sqrt(pow(rW, 2) + pow(gW, 2) + pow(bW, 2))
 
+# (R, G, B) average for red from our tests for the DELIVERY sensor
+rRDel = 233
+gRDel = 30
+bRDel = 30
+normrRDel = rRDel / sqrt(pow(rRDel, 2) + pow(gRDel, 2) + pow(bRDel, 2))
+rrStDev = 0.11983087
+normgRDel = gRDel / sqrt(pow(rRDel, 2) + pow(gRDel, 2) + pow(bRDel, 2))
+rgStDev =  0.5255912
+normbRDel = bRDel / sqrt(pow(rRDel, 2) + pow(gRDel, 2) + pow(bRDel, 2))
+rbStDev = 0.3402486
+
+
+# (R, G, B) average for orange from our tests for the DELIVERY sensor
+rODel = 200
+gODel = 70
+bODel = 20
+normrODel = rODel / sqrt(pow(rODel, 2) + pow(gODel, 2) + pow(bODel, 2))
+#orStDev = 
+normgODel = gODel / sqrt(pow(rODel, 2) + pow(gODel, 2) + pow(bODel, 2))
+#ogStDev =
+normbODel = bODel / sqrt(pow(rODel, 2) + pow(gODel, 2) + pow(bODel, 2))
+#obStDev =
+
+
+# (R, G, B) average for yellow from our tests for the DELIVERY sensor
+rYDel = 166
+gYDel = 130
+bYDel = 30
+normrYDel = rYDel / sqrt(pow(rYDel, 2) + pow(gYDel, 2) + pow(bYDel, 2))
+normgYDel = gYDel / sqrt(pow(rYDel, 2) + pow(gYDel, 2) + pow(bYDel, 2))
+normbYDel = bYDel / sqrt(pow(rYDel, 2) + pow(gYDel, 2) + pow(bYDel, 2))
+
+# (R, G, B) average for green from our tests for the DELIVERY sensor
+rGDel = 50
+gGDel= 107
+bGDel= 30
+normrGDel = rGDel / sqrt(pow(rGDel, 2) + pow(gGDel, 2) + pow(bGDel, 2))
+normgGDel = gGDel / sqrt(pow(rGDel, 2) + pow(gGDel, 2) + pow(bGDel, 2))
+normbGDel = bGDel / sqrt(pow(rGDel, 2) + pow(gGDel, 2) + pow(bGDel, 2))
+
+# (R, G, B) average for blue from our tests for the DELIVERY sensor
+rBDel = 30
+gBDel = 50
+bBDel = 80.4
+normrBDel = rBDel / sqrt(pow(rBDel, 2) + pow(gBDel, 2) + pow(bBDel, 2))
+normgBDel = gBDel / sqrt(pow(rBDel, 2) + pow(gBDel, 2) + pow(bBDel, 2))
+normbBDel = bBDel / sqrt(pow(rBDel, 2) + pow(gBDel, 2) + pow(bBDel, 2))
+
+# (R, G, B) average for purple from our tests for the DELIVERY sensor
+rPDel = 169.76
+gPDel = 28.75
+bPDel = 46.135
+normrPDel = rPDel / sqrt(pow(rPDel, 2) + pow(gPDel, 2) + pow(bPDel, 2))
+prStDev = 0.00378445
+normgPDel = gPDel / sqrt(pow(rPDel, 2) + pow(gPDel, 2) + pow(bPDel, 2))
+pgStDev = 0.0112719
+normbPDel = bPDel / sqrt(pow(rPDel, 2) + pow(gPDel, 2) + pow(bPDel, 2))
+pbStDev = 0.0090617
+
+# (R, G, B) average for white from our tests for the DELIVERY sensor
 rWDel = 301
 gWDel = 289
 bWDel = 200
@@ -131,7 +145,6 @@ def color_detection_drive(rgb):
   rgbBlue = [normrB,normgB,normbB]
   rgbPurple = [normrP,normgP,normbP]
   rgbWhite = [normrW, normgW, normbW]
-
 
   #calculating distance
   distanceFromRed = pow(pow(rgb[0]-rgbRed[0], 2) + pow(rgb[1]-rgbRed[1],2) + pow(rgb[2]-rgbRed[2],2),0.5) 
@@ -168,31 +181,47 @@ def color_detection_delivery(rgb):
          rgb[2] /pow((pow(rgb[0],2)+pow(rgb[1], 2)+pow(rgb[2],2)), 0.5))
   
   rgbRed = [normrRDel, normgRDel, normbRDel]
+  rgbRedStDev = [rrStDev, rgStDev, rbStDev]
   rgbOrange = [normrODel,normgODel,normbODel]
   rgbYellow = [normrYDel,normgYDel,normbYDel]
   rgbGreen = [normrGDel,normgGDel,normbGDel]
   rgbBlue = [normrBDel,normgB,normbBDel]
   rgbPurple = [normrPDel,normgPDel,normbPDel]
+  rgbPurpleStDev = [prStDev, pgStDev, pgStDev]
   rgbWhite = [normrWDel, normgWDel, normbWDel]
 
 
   #calculating distance
+  farDistanceFromRed = pow(pow(rgb[0]-rgbRed[0] + (5*rgbRedStDev[0]), 2) + pow(rgb[1]-rgbRed[1] + (5*rgbRedStDev[1]),2) + pow(rgb[2]-rgbRed[2] + (5*rgbRedStDev[2]),2),0.5) 
   distanceFromRed = pow(pow(rgb[0]-rgbRed[0], 2) + pow(rgb[1]-rgbRed[1],2) + pow(rgb[2]-rgbRed[2],2),0.5) 
+  smallDistanceFromRed = pow(pow(rgb[0]-rgbRed[0] - (5*rgbRedStDev[0]), 2) + pow(rgb[1]-rgbRed[1] - (5*rgbRedStDev[1]),2) + pow(rgb[2]-rgbRed[2] - (5*rgbRedStDev[2]),2),0.5) 
+
   distanceFromOrange = pow(pow(rgb[0]-rgbOrange[0], 2) + pow(rgb[1]-rgbOrange[1],2) + pow(rgb[2]-rgbOrange[2],2), 0.5) 
+
   distanceFromYellow = pow(pow(rgb[0]-rgbYellow[0], 2) + pow(rgb[1]-rgbYellow[1],2) + pow(rgb[2]-rgbYellow[2],2), 0.5) 
+
   distanceFromGreen = pow(pow(rgb[0]-rgbGreen[0], 2) + pow(rgb[1]-rgbGreen[1],2) + pow(rgb[2]-rgbGreen[2],2), 0.5) 
-  distanceFromBlue = pow(pow(rgb[0]-rgbBlue[0], 2) + pow(rgb[1]-rgbBlue[1],2) + pow(rgb[2]-rgbBlue[2],2), 0.5) 
+
+  distanceFromBlue = pow(pow(rgb[0]-rgbBlue[0], 2) + pow(rgb[1]-rgbBlue[1],2) + pow(rgb[2]-rgbBlue[2],2), 0.5)
+
+  farDistanceFromPurple = pow(pow(rgb[0]-rgbPurple[0] + (5*rgbPurpleStDev[0]), 2) + pow(rgb[1]-rgbPurple[1] - (5*rgbPurpleStDev[1]),2) + pow(rgb[2]-rgbPurple[2] - (5*rgbPurpleStDev[2]),2), 0.5)  
   distanceFromPurple = pow(pow(rgb[0]-rgbPurple[0], 2) + pow(rgb[1]-rgbPurple[1],2) + pow(rgb[2]-rgbPurple[2],2), 0.5) 
+  smallDistanceFromPurple = pow(pow(rgb[0]-rgbPurple[0] - (5*rgbPurpleStDev[0]), 2) + pow(rgb[1]-rgbPurple[1] - (5*rgbPurpleStDev[1]),2) + pow(rgb[2]-rgbPurple[2] - (5*rgbPurpleStDev[2]),2), 0.5) 
+
   distanceFromWhite = pow(pow(rgb[0]-rgbWhite[0], 2) + pow(rgb[1]-rgbWhite[1],2) + pow(rgb[2]-rgbWhite[2],2), 0.5) 
 
   distances = {
     "white" : distanceFromWhite,
+    "red" : smallDistanceFromRed,
     "red": distanceFromRed,
+    "red" : farDistanceFromRed,
     "orange": distanceFromOrange,
     "yellow": distanceFromYellow,
     "green" : distanceFromGreen,
     "blue" : distanceFromBlue, 
-    "purple" : distanceFromPurple 
+    "purple" : smallDistanceFromPurple,
+    "purple" : distanceFromPurple,
+    "purple" :farDistanceFromPurple
   }
 
   # find the closest distance from all the color distances; this is the color that we've detected  
